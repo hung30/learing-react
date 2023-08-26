e258b84db6a2f1f033a96e020555bbf998f6147e
hung30 <hungbmt303@gmail.com>
2023-08-26 17:17:16 +0700

    hello

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ExpenseItem from './components/ExpenseItem'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2>Lets get started</h2>
+        <ExpenseItem />
+      </div>
+    </>
+  )
+}
+
+export default App
